9bc929e558fe08af4b23ad8d6d012ff3e53ca78f
xing-6464 <xing_6464@foxmail.com>
2023-08-27 22:00:34 +0800

    button component init

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
-
-  </div>
+  <button class="vx-button"
+    :class="{ [`vk-button--${type}`]: type, 'is-plain': plain, 'is-round': round, 'is-circle': circle, 'is-disabled': disabled }"
+    :disabled="disabled">
+    <span>
+      <slot />
+    </span>
+  </button>
 </template>
 
 <script setup lang="ts">
+import type { ButtonProps } from './types'
 
+defineOptions({
+  name: 'VxButton'
+})
+defineProps<ButtonProps>()
 </script>
 
 <style scoped></style>
