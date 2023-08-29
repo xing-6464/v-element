a8b5f8e0e0cc202f234e30e04fb28f51cb9056f8
xing-6464 <xing_6464@foxmail.com>
2023-08-30 01:04:17 +0800

    报错

--- src/components/Collapse/Collapse.vue
+++ src/components/Collapse/Collapse.vue
@@ -5,7 +5,26 @@
 </template>
 
 <script setup lang="ts">
+import { ref, provide } from 'vue'
+import type { NameType } from './types'
+import { collapseContextKey } from './types'
+
 defineOptions({
   name: 'XCollapse',
+})
+const activeNames = ref<NameType[]>()
+
+const handleItemClick = (item: NameType) => {
+  const index = activeNames.value.indexOf(item)
+  if (index > -1) {
+    activeNames.value.splice(index, 1)
+  } else {
+    activeNames.value.push(item)
+  }
+}
+
+provide(collapseContextKey, {
+  activeNames,
+  handleItemClick
 })
 </script>
--- src/components/Collapse/CollapseItem.vue
+++ src/components/Collapse/CollapseItem.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="x-collapse-item" :class="{ 'is-disabled': disabled }">
-    <div class="x-collapse-item__header" :id="`item-header-${name}`">
+    <div class="x-collapse-item__header" :id="`item-header-${name}`" @click="handleClick">
       <slot name="title">{{ title }}</slot>
     </div>
-    <div class="x-collapse-item__content" :id="`item-content-${name}`">
+    <div class="x-collapse-item__content" :id="`item-content-${name}`" v-show="isActive">
       <slot />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { inject, computed } from 'vue'
 import type { CollapseItemProps } from './types'
+import { collapseContextKey } from './types'
 
 defineOptions({
   name: 'XCollapseItem',
 })
-defineProps<CollapseItemProps>()
+const props = defineProps<CollapseItemProps>()
+const collapseContext = inject(collapseContextKey)
+const isActive = computed(() => collapseContext?.activeNames.value.includes(props.name))
+const handleClick = () => {
+  if (props.disabled) return
+  collapseContext?.handleItemClick(props.name)
+}
 </script>
+
+<style>
+.x-collapse-item__header {
+  font-size: 30px;
+}
+</style>
