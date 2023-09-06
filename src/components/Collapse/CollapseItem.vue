665dbb298f1f3eb1014abfa080a5ee25e7b9b657
xing-6464 <xing_6464@foxmail.com>
2023-09-06 16:14:05 +0800

    Collapse 添加下拉动画 - 动态计算高度

--- src/components/Collapse/CollapseItem.vue
+++ src/components/Collapse/CollapseItem.vue
@@ -4,7 +4,7 @@
       :id="`item-header-${name}`" @click="handleClick">
       <slot name="title">{{ title }}</slot>
     </div>
-    <Transition name="fade">
+    <Transition name="slide" v-on="transitionEvents">
       <div class="x-collapse-item__content" :id="`item-content-${name}`" v-show="isActive">
         <slot />
       </div>
@@ -27,6 +27,27 @@ const isActive = computed(() => collapseContext?.activeNames.value.includes(prop
 const handleClick = () => {
   if (props.disabled) return
   collapseContext?.handleItemClick(props.name)
+}
+
+const transitionEvents: Record<string, (el: HTMLElement) => void> = {
+  beforeEnter(el) {
+    el.style.height = '0px'
+  },
+  enter(el) {
+    el.style.height = `${el.scrollHeight}px`
+  },
+  afterEnter(el) {
+    el.style.height = ''
+  },
+  beforeLeave(el) {
+    el.style.height = `${el.scrollHeight}px`
+  },
+  leave(el) {
+    el.style.height = '0px'
+  },
+  afterLeave(el) {
+    el.style.height = ''
+  }
 }
 </script>
 
