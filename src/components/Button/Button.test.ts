cb13a3652755ed1c208e0ce7125fc9fe91074e7f
xing-6464 <xing_6464@foxmail.com>
2023-09-06 22:27:27 +0800

    Button 组件 测试

--- src/components/Button/Button.test.ts
+++ src/components/Button/Button.test.ts
@@ -1,4 +1,4 @@
-import { describe, test } from 'vitest'
+import { describe, test, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import Button from './Button.vue'
 
@@ -14,5 +14,30 @@ describe('Button.vue', () => {
     })
 
     console.log(wrapper.html())
+    expect(wrapper.classes()).toContain('x-button--primary')
+    // slot
+    // get, find
+    expect(wrapper.get('button').text()).toBe('button')
+
+    wrapper.get('button').trigger('click')
+    console.log(wrapper.emitted())
+    expect(wrapper.emitted()).toHaveProperty('click')
+  })
+
+  test('disabled button', () => {
+    const wrapper = mount(Button, {
+      props: {
+        disabled: true
+      },
+      slots: {
+        default: 'button'
+      }
+    })
+    // attributes
+    expect(wrapper.attributes('disabled')).toBeDefined()
+    // attributes
+    expect(wrapper.find('button').element.disabled).toBeDefined()
+    wrapper.get('button').trigger('click')
+    expect(wrapper.emitted()).not.toHaveProperty('click')
   })
 })
