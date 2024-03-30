f149ab54e0d97f47e032b7c21c7bbadb50ba2d21
xing-6464 <17621404964@163.com>
2024-03-30 20:01:34 +0800

    Collapse 添加测试用例

---
+++ src/components/Collapse/Collapse.test.tsx
@@ -0,0 +1,62 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, test } from 'vitest'
+import Collapse from './Collapse.vue'
+import CollapseItem from './CollapseItem.vue'
+
+describe('Collapse.vue', async () => {
+  test('basic collapse', async () => {
+    const wrapper = mount(
+      () => (
+        <Collapse modelValue={['a']}>
+          <CollapseItem name="a" title="title a">
+            content a
+          </CollapseItem>
+          <CollapseItem name="b" title="title b">
+            content b
+          </CollapseItem>
+          <CollapseItem name="c" title="title c" disabled>
+            content c
+          </CollapseItem>
+        </Collapse>
+      ),
+      {
+        global: {
+          stubs: ['Icon']
+        },
+        attachTo: document.body
+      }
+    )
+
+    const headers = wrapper.findAll('.x-collapse-item__header')
+    const contents = wrapper.findAll('.x-collapse-item__wrapper')
+
+    // length
+    expect(headers.length).toBe(3)
+    expect(contents.length).toBe(3)
+
+    // text
+    const firstHeader = headers[0]
+    const secondeHeader = headers[1]
+    expect(firstHeader.text()).toBe('title a')
+
+    // content
+    const firstContent = contents[0]
+    const secondContent = contents[1]
+    const disabledContent = contents[2]
+    expect(firstContent.isVisible()).toBeTruthy()
+    expect(secondContent.isVisible()).toBeFalsy()
+    expect(firstContent.text()).toBe('content a')
+
+    // action
+    await firstHeader.trigger('click')
+    expect(firstContent.isVisible()).toBeFalsy()
+    await secondeHeader.trigger('click')
+    expect(secondContent.isVisible()).toBeTruthy()
+
+    // disabled
+    const disabledHeader = headers[2]
+    expect(disabledHeader.classes()).toContain('is-disabled')
+    await disabledHeader.trigger('click')
+    expect(disabledContent.isVisible()).toBeFalsy()
+  })
+})
