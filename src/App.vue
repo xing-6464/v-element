eca11617275effaa25bd918be40f5e1e1cdeb794
xing-6464 <17621404964@163.com>
2024-04-03 07:53:26 +0800

    Input 编码第一部分搭建框架

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from 'vue'
 import XButton from './components/Button/Button.vue'
 import Collapse from './components/Collapse/Collapse.vue'
 import Item from './components/Collapse/CollapseItem.vue'
 import { createMessage } from './components/Message/method'
 
-
 const openValue = ref(['a'])
 
-
 onMounted(() => {
-  createMessage({message: 'hello world', duration: 3000})
-  createMessage({message: 'hello world world', duration: 0, type: 'success', showClose: true})
-  createMessage({message: 'hello world world world', duration: 0, type: 'warning'})
+  createMessage({ message: 'hello world', duration: 3000 })
+  createMessage({ message: 'hello world world', duration: 0, type: 'success', showClose: true })
+  createMessage({ message: 'hello world world world', duration: 0, type: 'warning' })
 })
-
 </script>
 
 <template>
-  <!-- <Message message="hello message" :duration="0" :show-close="true" /> -->
   <main>
-    <XButton plain type="primary">Plain Test</XButton>
-    <XButton plain type="success">Plain Test</XButton>
-    <XButton plain type="info">Plain Test</XButton>
+    <XButton type="primary">Plain Test</XButton>
+    <XButton type="success">Plain Test</XButton>
+    <XButton type="info">Plain Test</XButton>
     <br />
     <br />
     <XButton size="large" loading>Loading</XButton>
---
+++ src/components/Input/Input.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    class="x-input"
+    :class="{
+      [`x-input-${type}`]: type,
+      [`x-input-${size}`]: size,
+      'is-disabled': disabled,
+      'is-prepend': $slots.prepend,
+      'is-append': $slots.append,
+      'is-prefix': $slots.prefix,
+      'is-suffix': $slots.suffix
+    }"
+  >
+    <!-- input -->
+    <template v-if="type !== 'textarea'">
+      <div v-if="$slots.prepped" class="x-input__prepend">
+        <slot name="prepend" />
+      </div>
+      <div class="x-input__wrapper">
+        <!-- prefix slot -->
+        <span v-if="$slots.prefix" class="x-input__prefix">
+          <slot name="prefix" />
+        </span>
+        <input class="x-input__inner" :type="type" :disabled="disabled" />
+        <!-- suffix slot -->
+        <span v-if="$slots.suffix" class="x-input__suffix">
+          <slot name="suffix" />
+        </span>
+      </div>
+      <!-- append slot -->
+      <div v-if="$slots.append" class="x-input__append">
+        <slot name="append" />
+      </div>
+    </template>
+    <!-- textarea -->
+    <template v-else>
+      <textarea class="x-textarea__wrapper" :disabled="disabled" />
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { InputProps } from './types'
+
+const props = withDefaults(defineProps<InputProps>(), {
+  type: 'text'
+})
+</script>
